ba15659a57673ca52791d17455fa387dfab4fb49
pinterroniaron <pinter.roni.aron@students.jedlik.eu>
2025-02-06 11:20:43 +0100

    api fetch

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const url = 'https://ali-express1.p.rapidapi.com/categories';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '6d53da9344mshb02c9676f6a58c0p1d1dc5jsncbde29faef2c',
+		'x-rapidapi-host': 'ali-express1.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+
 const cardContainer = document.querySelector('.card-container');
 
   cardContainer.addEventListener('wheel', (event) => {
