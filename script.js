bcbc3ecea46929613572ae1b06ad7bd2970654ac
pinterroniaron <pinter.roni.aron@students.jedlik.eu>
2025-02-10 15:29:55 +0100

    card overflow half fixed and responsive cards

--- script.js
+++ script.js
@@ -80,9 +80,14 @@ catch (error) {
 
 
 
-const cardContainer = document.querySelector('.card-container');
+const cardContainer = document.querySelector('.vertical-scroll');
 
-cardContainer.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  cardContainer.scrollLeft += (event.deltaY * 8);
-});
+if (!window.matchMedia("(max-width: 1200px)").matches) {
+  cardContainer.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    cardContainer.scrollLeft += (event.deltaY * 8);
+  });
+
+}
+
+
