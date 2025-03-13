696f96503c7f0c825160d15444656c5a9586d3bc
pinterroniaron <pinter.roni.aron@students.jedlik.eu>
2025-03-13 10:11:04 +0100

    done

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,6 @@
 let darkmode = localStorage.getItem('darkmode')
 const themeToggle = document.getElementById('theme-toggle')
+const themeToggle2 = document.getElementById('theme-toggle2')
 
 const enableDarkmode = () => {
     document.body.classList.add('darkmode');
@@ -14,6 +15,11 @@ const disableDarkmode = () => {
 if(darkmode === "active") enableDarkmode()
 
 themeToggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+themeToggle2.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
